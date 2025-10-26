b13f9101ce2a700bf9b4e5f9fac8e32ec2d71453
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2025-10-26 18:54:43 -0400

    navbar start

--- uf-psu/src/App.jsx
+++ uf-psu/src/App.jsx
@@ -1,19 +1,24 @@
 import Home from './pages/Home/Home'
-
+import "./App.css"
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import './App.css'
+import { NavBar } from './components/NavBar.jsx'
 
 function App() {
-
   return (
-    <>
+    /*<>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home/>} /> 
       </Routes>
     </BrowserRouter>
+    
     </>
+    */
+    <div>
+      <NavBar />
+    </div>
   )
 }
 
