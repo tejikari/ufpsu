05fac3d347b3fff94a1e68c3281a289e5192e26e
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2025-11-08 14:38:41 -0500

    navbarinital

--- uf-psu/src/App.jsx
+++ uf-psu/src/App.jsx
@@ -1,25 +1,14 @@
 import Home from './pages/Home/Home'
 import "./App.css"
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 import './App.css'
 import { NavBar } from './components/NavBar.jsx'
 
 function App() {
   return (
-    /*<>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} /> 
-      </Routes>
-    </BrowserRouter>
-    
-    </>
-    */
-    <div>
-      <NavBar />
-    </div>
-  )
+    <NavBar />
+  );
 }
 
 export default App
--- uf-psu/src/components/NavBar.jsx
+++ uf-psu/src/components/NavBar.jsx
@@ -1,7 +1,16 @@
 import React from "react"
+import psuLogo from '../assets/psu_logo.png'
+import './NavBar.css'
+import { Link } from "react-router-dom"
 
 export const NavBar = () => {
-    return (
-        <div>Navbar</div>
-    )
+    return <nav>
+        <ul>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/events">Events</Link></li>
+            <li><Link to="/"><img id="logo" src={psuLogo} alt="PSU logo"/></Link></li>
+            <li><Link to="/board">Board</Link></li>
+            <li><Link to="/statements">Statements</Link></li>
+        </ul>
+    </nav>
 }
