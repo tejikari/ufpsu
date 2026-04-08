f47d8cae59acd4d3e6f62af688ba18ac1241d27f
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2026-04-08 10:34:38 -0400

    contact page update

--- uf-psu/src/pages/Contact/Contact.jsx
+++ uf-psu/src/pages/Contact/Contact.jsx
@@ -3,10 +3,16 @@ import './Contact.css'
 const Contact = () => {
 	return (
 		<div className="contact">
-			<h1>Contact Us</h1>
-			<p>ufpsu@gmail.com</p>
-			<p>@ufpsu</p>
-			<p>Join Our Discord</p>
+			<div>
+				<h1>Contact Us</h1>
+				<p>ufpsu@gmail.com</p>
+				<p>@ufpsu</p>
+				<p>Join Our Discord</p>
+			</div>
+			<div>
+				<h1>Support PSU!</h1>
+				<p>Venmo: @venmo</p>
+			</div>
 		</div>
 	)
 }
