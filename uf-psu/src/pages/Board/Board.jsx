42bdf76ea0610a4d41d24f2531d75ca0be460a50
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2026-04-08 12:06:52 -0400

    board

--- uf-psu/src/pages/Board/Board.jsx
+++ uf-psu/src/pages/Board/Board.jsx
@@ -2,9 +2,17 @@ import './Board.css'
 
 const Board = () => {
 	return (
-		<h1 className='board-section-text'>
-			Board
-		</h1>
+		<div className="board">
+			<div>
+			<h2>Executive Board</h2>
+			<h3>President</h3>
+			<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+				<rect width="240" height="270" fill="#696969" />
+			</svg>
+				<h3>Name</h3>
+				<h4>they/them</h4>
+			</div>
+		</div>
 	)
 }
 
