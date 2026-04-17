5fc1c1705fb156adf697b1e5fe857b20eee1e73a
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2026-04-17 12:13:20 -0400

    Fixed css conflicts, added statements & events
    added hover color changing links
    fixed css conflicts between pages

--- uf-psu/src/pages/Events/Events.jsx
+++ uf-psu/src/pages/Events/Events.jsx
@@ -2,9 +2,50 @@ import './Events.css'
 
 const Events = () => {
 	return (
-		<h1 className='events-section-text'>
-			Events
-		</h1>
+		<div className="events">
+            <div>
+				<h2>Next Event</h2>
+				<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+					<rect width="500" height="500" fill="#696969" />
+				</svg>
+			</div>
+			<div>
+				<h2>Calender</h2>
+				<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+					<rect width="500" height="500" fill="#696969" />
+				</svg>
+			</div>
+            <div><h2>Previous Events</h2></div>
+            <div class="flex-container">
+				<svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+			</div>
+        </div>
 	)
 }
 
--- uf-psu/src/pages/Statements/Statements.jsx
+++ uf-psu/src/pages/Statements/Statements.jsx
@@ -1,8 +1,45 @@
+import './Statements.css'
+
 const Statements = () => {
     return (
-        <h1 className='statements-section-text'>
-            Statements
-        </h1>
+        <div className="statements">
+            <div>
+				<h2>Recent</h2>
+				<svg width="500" height="500" xmlns="http://www.w3.org/2000/svg">
+					<rect width="500" height="500" fill="#696969" />
+				</svg>
+			</div>
+            <div><h2>Previous Statements</h2></div>
+            <div class="flex-container">
+				<svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+                <svg class="item" width="240" height="240" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="240" fill="#696969" />
+				</svg>
+			</div>
+        </div>
     )
 }
 export default Statements
