751e5898e279521b8e93afe529d4fa804f3fb665
Amy Hoang <117518245+TiniToni@users.noreply.github.com>
2026-04-17 10:55:03 -0400

    Board page placeholders placed

--- uf-psu/src/pages/Board/Board.jsx
+++ uf-psu/src/pages/Board/Board.jsx
@@ -4,13 +4,277 @@ const Board = () => {
 	return (
 		<div className="board">
 			<div>
-			<h2>Executive Board</h2>
-			<h3>President</h3>
-			<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
-				<rect width="240" height="270" fill="#696969" />
-			</svg>
+				<h2>Executive Board</h2>
+				<h3>President</h3>
+				<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="270" fill="#696969" />
+				</svg>
 				<h3>Name</h3>
 				<h4>they/them</h4>
+			</div>
+			{/*eboard row 2 */}
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+						<h3>Name</h3>
+						<h4>they/them</h4>
+				</div>
+			</div>
+			{/*eboard row 3 */}
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+						<h3>Name</h3>
+						<h4>they/them</h4>
+				</div>
+			</div>
+			{/*admin */}
+			<div><h2>Admin</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+						<h3>Name</h3>
+						<h4>they/them</h4>
+				</div>
+			</div>
+			{/*external */}
+			<div><h2>External</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			{/*Finance */}
+			<div><h2>Finance</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			{/*Internal */}
+			<div><h2>Internal</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			{/*Communications */}
+			<div><h2>Communications</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			{/*Communications row 2*/}
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			{/*Political affairls*/}
+			<div><h2>Political Affairs</h2></div>
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+			</div>
+			<div>
+				<h2>Queer Cultivation Program (QCP)</h2>
+				<h3>President</h3>
+				<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+					<rect width="240" height="270" fill="#696969" />
+				</svg>
+				<h3>Name</h3>
+				<h4>they/them</h4>
+			</div>
+			{/*qcp board row 2 */}
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+						<h3>Name</h3>
+						<h4>they/them</h4>
+				</div>
+			</div>
+			{/*qcp board row 3 */}
+			<div class="flex-container">
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+					<h3>Name</h3>
+					<h4>they/them</h4>
+				</div>
+				<div>
+					<h3>President</h3>
+					<svg width="240" height="270" xmlns="http://www.w3.org/2000/svg">
+						<rect width="240" height="270" fill="#696969" />
+					</svg>
+						<h3>Name</h3>
+						<h4>they/them</h4>
+				</div>
 			</div>
 		</div>
 	)
